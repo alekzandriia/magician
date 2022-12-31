3f1d85d399f55bbf6d234023346234c4f100a6a2
Alekzandriia <alekzoreillyz@me.com>
2022-12-31 17:33:25 -0500

    forgot to update heh 😅

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const useUppercase = document.getElementById('uppercase')
 const useNumbers = document.getElementById('numbers')
 const useSymbols = document.getElementById('symbols')
 const generateBtn = document.getElementById('generate')
+const strength = document.getElementById('strength')
 
 // Show user selected character length
 const lengthNum = document.getElementById('lengthNum')
-function updateSlider() {
+lengthNum.textContent = length.value;
+length.addEventListener('input', () => {
   lengthNum.textContent = length.value;
-}
-length.addEventListener('input', updateSlider)
-updateSlider()
+})
 
 // Character Bank 
 const numbers = [1,2,3,4,5,6,7,8,9,0]
@@ -22,29 +22,69 @@ const charCodes = Array.from(Array(26)).map((_, i) => i + 97)
 const lowerCaseLetters = charCodes.map(code => String.fromCharCode(code))
 const upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase())
 
-
-//  Generate Password
-
-function generatePassword(length, useLowercase, useUppercase, useNumbers, useSymbols ) {
-  const availableCharacters = [
-    ...(useNumbers ? numbers : []),
-    ...(useSymbols ? symbols : []),
-    ...(useUppercase ? upperCaseLetters : []),
-    ...(useLowercase ? lowerCaseLetters : []),
-  ]
-
-  let newPassword = ""
-  
-  for (let i = 0; i < length; i++) {
-    let index = Math.floor(Math.random() * availableCharacters.length)
-    newPassword += availableCharacters[index]
+// Get Random Char from Array
+function getRandomChar(arr) {
+  let index = Math.floor(Math.random() * arr.length)
+  return arr[index]
+}
+// generate Password
+function generatePassword() {
+let generatedPassword = ""
+for (let i = 0; i < length.value ; i++) {
+  if (useLowercase.checked) {
+    generatedPassword += getRandomChar(lowerCaseLetters)
   }
-  console.log(newPassword)
+  if (useUppercase.checked) {
+    generatedPassword += getRandomChar(upperCaseLetters)
+  }
+  if (useNumbers.checked) {
+    generatedPassword += getRandomChar(numbers)
+  }
+  if (useSymbols.checked) {
+    generatedPassword += getRandomChar(symbols)
+  }
+}
+let finalPassword = generatedPassword.slice(0, length.value);
+return finalPassword;
 }
 
-
 // Button Functionality
+generateBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  password.value = null
+  password.value = generatePassword()
+  calculateStrength()
+})
 
-generateBtn.addEventListener('click', generatePassword )
-
-generatePassword()
+// Strength indicator
+function calculateStrength() {
+let strengthCounter = 0;
+if (password.value && length.value >= 12) {
+  strengthCounter++
+}
+if (useLowercase.checked) {
+  strengthCounter++
+}
+if (useUppercase.checked) {
+  strengthCounter++
+}
+if (useNumbers.checked) {
+  strengthCounter++
+}
+if (useSymbols.checked) {
+  strengthCounter++
+}
+  if (strengthCounter === 0) {
+  strength.textContent = ""
+} else if (strengthCounter === 1) {
+  strength.textContent = "very weak"
+} else if (strengthCounter === 2) {
+  strength.textContent = "weak"
+} else if (strengthCounter === 3) {
+  strength.textContent = "medium"
+} else if (strengthCounter === 4) {
+  strength.textContent = "strong"
+} else if (strengthCounter === 5) {
+  strength.textContent = "very strong"
+}
+}
